b3a3916438a01bc9aba8380702ea914e24e49865
TausifTKhan <tausiftkhan7@gmail.com>
2025-02-09 20:31:49 -0500

    added code from firebase console

---
+++ frontend/firebase-config.js
@@ -0,0 +1,21 @@
+
+//from firebase console
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDSVrwBNAsbsOfAGbF_4cwdj843YUQnQmM",
+  authDomain: "stepverification-e296b.firebaseapp.com",
+  projectId: "stepverification-e296b",
+  storageBucket: "stepverification-e296b.firebasestorage.app",
+  messagingSenderId: "500001490535",
+  appId: "1:500001490535:web:91f1b958c1dc065aaef6c2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
