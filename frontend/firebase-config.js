9d5310f8358663f1c016732e77ae8b5943cf04a7
TausifTKhan <tausiftkhan7@gmail.com>
2025-02-11 16:38:21 -0500

    added code to html and javascript files

--- frontend/firebase-config.js
+++ frontend/firebase-config.js
@@ -1,38 +1,46 @@
-
+import { initializeApp } from "firebase/app";
+import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 //from firebase console
 
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDSVrwBNAsbsOfAGbF_4cwdj843YUQnQmM",
-  authDomain: "stepverification-e296b.firebaseapp.com",
-  projectId: "stepverification-e296b",
-  storageBucket: "stepverification-e296b.firebasestorage.app",
-  messagingSenderId: "500001490535",
-  appId: "1:500001490535:web:91f1b958c1dc065aaef6c2"
+    apiKey: "AIzaSyCf0qCtrXWOB6zFe36qxrxiV30HA2kEJas",
+    authDomain: "wayne-state-marketshare.firebaseapp.com",
+    projectId: "wayne-state-marketshare",
+    storageBucket: "wayne-state-marketshare.firebasestorage.app",
+    messagingSenderId: "11946478991",
+    appId: "1:11946478991:web:a2382361767bb0a5e54ffa",
+    measurementId: "G-FCRMC500EK"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-window.recaptchaVerifier = new RecaptchaVerifier('sendCodeBtn', { 'size': 'invisible', 'callback': (response) => {
+window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', { 'size': 'invisible', 'callback': (response) => {
     console.log("reCAPTCHA resolved", response);
+},
+'expired-callback': () => {
+    console.log("reCAPTCHA expired");
+},
+'error-callback': (error) => {
+    console.error("reCAPTCHA error", error);
 }
 }, auth);
 
 //sends code to phone
 function sendVerificationCode(phoneNumber){
+    if(!phoneNumber.startsWith("+")){
+        phoneNumber = "+1" + phoneNumber;
+    }
+    
     const appVerifier = window.recaptchaVerifier;
     signInWithPhoneNumber(auth, phoneNumber, appVerifier)
-    .then((confirmationResults) => {
+    .then((confirmationResult) => {
+        window.confirmationResult = confirmationResult;
         const code = prompt('Enter the code sent to your phone number');
-        return confirmationResults.confirm(code);
+        return confirmationResult.confirm(code);
     })
     .then((result) => {
         alert("Account Verified Successfully!");
