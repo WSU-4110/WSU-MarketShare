ecb1b37d60f6116273906086880dc415674b0b64
hmusaab <hm0060@wayne.edu>
2025-02-10 19:44:26 -0800

    New updated login.js

--- frontend/login.js
+++ frontend/login.js
@@ -1,14 +1,17 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCf0qCtrXWOB6zFe36qxrxiV30HA2kEJas",
-    authDomain: "wayne-state-marketshare.firebaseapp.com",
-    projectId: "wayne-state-marketshare",
-    storageBucket: "wayne-state-marketshare.firebasestorage.app",
-    messagingSenderId: "11946478991",
-    appId: "1:11946478991:web:a2382361767bb0a5e54ffa",
-    measurementId: "G-FCRMC500EK"
-  };
 
-const app = firebase.initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyCf0qCtrXWOB6zFe36qxrxiV30HA2kEJas",
+  authDomain: "wayne-state-marketshare.firebaseapp.com",
+  projectId: "wayne-state-marketshare",
+  storageBucket: "wayne-state-marketshare.firebasestorage.app",
+  messagingSenderId: "11946478991",
+  appId: "1:11946478991:web:a2382361767bb0a5e54ffa",
+  measurementId: "G-FCRMC500EK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = firebase.database();
 const auth = firebase.auth();
 
@@ -30,12 +33,20 @@ function loginUser(){
   const Password = document.getElementById("password").value;
   auth.signInWithAccessIDandPassword(AccessID,Password)
   .then((userCredential) => {
-    alert("Login Successful");
+    window.location.href = "FrontPage.html";
   })
   .catch((error) => {
     alert("Error, login not successful" + error.message);
   })
 }
+
+auth.onAuthStateChanged((user) => {
+  if (user) {
+      console.log("User is logged in:", user.email);
+  } else {
+      console.log("No user is logged in.");
+  }
+});
 
 function SaveUser(userID, AccessID){
   db.ref('users/' + userID).set({
