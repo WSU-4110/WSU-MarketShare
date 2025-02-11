f69e9adfb826fa80a1e41e3332a451341727f695
Musaab Hafiz <141362400+hmusaab@users.noreply.github.com>
2025-02-10 19:55:48 -0800

    Update login.js
    Updated

--- frontend/login.js
+++ frontend/login.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCf0qCtrXWOB6zFe36qxrxiV30HA2kEJas",
-  authDomain: "wayne-state-marketshare.firebaseapp.com",
-  projectId: "wayne-state-marketshare",
-  storageBucket: "wayne-state-marketshare.firebasestorage.app",
-  messagingSenderId: "11946478991",
-  appId: "1:11946478991:web:a2382361767bb0a5e54ffa",
-  measurementId: "G-FCRMC500EK"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-=======
 const firebaseConfig = {
     apiKey: "AIzaSyCf0qCtrXWOB6zFe36qxrxiV30HA2kEJas",
     authDomain: "wayne-state-marketshare.firebaseapp.com",
@@ -25,7 +9,7 @@ const firebaseConfig = {
   };
 
 const app = firebase.initializeApp(firebaseConfig);
->>>>>>> a2c02dd59988f4d45f6c814f8fe6f995b979eda1
+const analytics = getAnalytics(app);
 const db = firebase.database();
 const auth = firebase.auth();
 
@@ -47,18 +31,14 @@ function loginUser(){
   const Password = document.getElementById("password").value;
   auth.signInWithAccessIDandPassword(AccessID,Password)
   .then((userCredential) => {
-<<<<<<< HEAD
     window.location.href = "FrontPage.html";
-=======
-    alert("Login Successful");
->>>>>>> a2c02dd59988f4d45f6c814f8fe6f995b979eda1
   })
   .catch((error) => {
     alert("Error, login not successful" + error.message);
   })
 }
 
-<<<<<<< HEAD
+
 auth.onAuthStateChanged((user) => {
   if (user) {
       console.log("User is logged in:", user.email);
@@ -67,8 +47,6 @@ auth.onAuthStateChanged((user) => {
   }
 });
 
-=======
->>>>>>> a2c02dd59988f4d45f6c814f8fe6f995b979eda1
 function SaveUser(userID, AccessID){
   db.ref('users/' + userID).set({
     email: email, 
