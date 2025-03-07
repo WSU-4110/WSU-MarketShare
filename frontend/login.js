cdc7499589fcf7ba578f1e83e52909d9ec02cc00
Musaab Hafiz <141362400+hmusaab@users.noreply.github.com>
2025-03-07 15:04:23 -0800

    Update login.js

--- frontend/login.js
+++ frontend/login.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, sendEmailVerification } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
 
 // Firebase configuration
 const firebaseConfig = {
