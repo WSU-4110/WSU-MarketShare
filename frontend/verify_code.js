841cf6e708ab0eaa022da229acf6381b01c99e2d
Musaab Hafiz <141362400+hmusaab@users.noreply.github.com>
2025-04-15 17:03:55 -0400

    Update verify_code.js

--- frontend/verify_code.js
+++ frontend/verify_code.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
 import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
+import emailjs from 'https://cdn.jsdelivr.net/npm/@emailjs/browser@3.11.0/+esm';
+
 
 // Firebase config
 const firebaseConfig = {
