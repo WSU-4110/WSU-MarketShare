fd7b551a6e8b7792ecad2c03da8f8f4c548f69ad
TausifTKhan <tausiftkhan7@gmail.com>
2025-04-16 23:24:42 -0400

    listener methods

--- frontend/Cart.js
+++ frontend/Cart.js
@@ -4,4 +4,15 @@ class Cart {
         this.userID = null;
         this.initCart();
     }
+    async initCart(){
+    const user = await this.getCurrenntUserAsync();
+    if(!user){
+        console.log('no user logged in');
+        window.location.href = "login.html";
+        return;
+    }
+    this.userId = user.uid;
+    this.setupFirebaseListener();
+    this.initEventListeners();
+    }
 }
